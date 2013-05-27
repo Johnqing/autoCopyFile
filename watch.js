f6216de03a293021a96e5076a5c768ea581f84c3
刘卿 <csssnow@gmail.com>
2013-05-27 18:13:45 +0800

    Update watch.js

--- watch.js
+++ watch.js
@@ -22,8 +22,8 @@ function baseCopyFile(src, dst){
 
 var copyFile = function(filename, basepath, copypath) {
     var name = filename.split(basepath)[1];
+    console.log(name);
     baseCopyFile(filename, copypath + name);
-    console.log('success!');
 };
 /**
  * 基础路径获取
@@ -33,16 +33,21 @@ fs.readFile('package.json', function(err, data){
     if(err) throw err;
     var package = JSON.parse(data),
         basepath = package.basePath,
+        isAutoCopy = package.isAutoCopy,
         copyPath = package.copyPath;
     // walk配置
     var walker  = walk.walk(basepath, { followLinks: false });
 
     walker.on('file', function(root, fileStats, next) {
         (function(filename){
-            copyFile(filename, basepath, copyPath);
-            fs.watch(filename ,function(event, name){
+
+            fs.watch(filename,function(event, name){
                 if(event === 'change') {
                     copyFile(filename, basepath, copyPath);
+                }
+                console.log(isAutoCopy);
+                if(isAutoCopy){
+                    fs.unwatch(filename);
                 }
             });
         })(root + '/' + fileStats.name);
